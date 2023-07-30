f60543ef88faaee8d0d375f0c11c494411a6e49f
Najma Jama <Najma55.nj@gmail.com>
2023-07-30 17:25:47 -0500

    corrected some mistakes

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,13 +1,13 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = 'Najma '
+let firstName = 'Najma'
 console.log(firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Jama';
 console.log(lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = firstName + lastName;
+let fullName = firstName + ' ' + lastName;
 // 4 - Console log the value of `fullName`
 console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
@@ -33,12 +33,14 @@ console.log(friendsPets)
 pets+=2;
 console.log(pets)
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-let allowedPets = 5
+const allowedPets = 5;
 console.log(allowedPets);
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-if ('adventurous == true') {
-    console.log("Adventures are great!");
+if (adventurous === true) {
+    console.log('Adeventures are great!');
+} else {
+    console.log('How about we stay home?');
 }
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
